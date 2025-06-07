4bbd0c6499103a7228c2597d9ef229e75f046972
anamancilha <anabeatrizmancilha@gmail.com>
2025-06-07 13:44:36 -0300

    ProjetoA3
    Projeto da A3.
    Solução golpe do presente

---
+++ A3/A3Bradesco/src/Projeto/ContaBanco.java
@@ -0,0 +1,11 @@
+package Projeto;
+
+/**
+ *
+ * @author Ana Mancilha
+ */
+public class ContaBanco extends Usuario{
+       private int numeroConta;
+       private int saldo;
+       private int dataAbertura;
+}
---
+++ A3/A3Bradesco/src/Projeto/Usuario.java
@@ -0,0 +1,14 @@
+package Projeto;
+
+/**
+ *
+ * @author Ana Mancilha
+ */
+public class Usuario {
+     String nome;
+     int CPF;
+     int RG;
+     int telefone;
+     String email;
+     boolean cliente;
+}
